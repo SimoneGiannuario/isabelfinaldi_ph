f9481d6efcf4e4c012bc4c4cb9d10c29069f0337
Simone Giannuario <simone.giannuario@external.eni.com>
2026-03-27 16:07:18 +0100

    fix: fixed photo quality

--- src/data/photos.ts
+++ src/data/photos.ts
@@ -262,7 +262,7 @@ export function getOptimizedUrl(src: string, width?: number): string {
 
   const params = [];
   if (width) params.push(`width=${width}`);
-  params.push(`quality=75`);
+  params.push(`quality=60`);
   params.push('format=auto');
 
   const base = imgDomain.replace(/\/$/, '');
